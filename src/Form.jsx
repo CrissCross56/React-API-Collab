53ca7613de44638305eb0c1f5e11f381f9b3b0d5
CrissCross56 <grant.terdoslavich@gmail.com>
2023-05-04 05:57:54 -0400

    removed unnecessary html

--- src/Form.jsx
+++ src/Form.jsx
@@ -53,12 +53,7 @@ function Form(){
         <div id="submissionForm">
             <h2>CRUD Form</h2>
 
-            <ul id="crudNav">
-                <li className="crudChoice">Create</li>
-                <li className="crudChoice">Read</li>
-                <li className="crudChoice">Update</li>
-                <li className="crudChoice">Delete</li>
-            </ul>
+         
 
             <form onSubmit={handleSubmit}>
             <input className="userWrite" onChange={handleChange} type="text" placeholder="title" id='title'/>
